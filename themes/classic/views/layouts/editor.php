6a5229895b0c960c7cd9764cdb998f9e85c3df05
Ruanceli N Santos <contato@ruanceli.com>
2012-04-18 16:55:40 -0300

    Adicionado o jquery e criado e setado o tema.

---
+++ themes/classic/views/layouts/editor.php
@@ -0,0 +1,27 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/jquery/jquery.ui.all.css" />
+                <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery/jquery-1.7.2.js"></script>
+                <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery/jquery.ui.core.js"></script>
+                <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery/jquery.ui.widget.js"></script>
+                <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/jquery/jquery.ui.button.js"></script>
+                <script>
+                    $(function() {
+                            $( "button").button();
+                    });
+                 </script>
+        <title></title>
+    </head>
+    <body>
+        <button>A button element</button>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
