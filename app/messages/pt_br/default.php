7aada8abd9d836e9e98d54014a32eac1f4678e57
Ruanceli N Santos <contato@ruanceli.com>
2012-06-14 20:40:11 -0300

    Arquivos de Tradução de Idioma

---
+++ app/messages/pt_br/default.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+    'List Users' => 'Listar Usuários',
+    'User Manager' => 'Gerenciamento de Usuários',
+    'Create a new user' => 'Criar um novo usuário',
+    'Fields with * are required.' => 'Campos com * sao obrigatórios',
+    'User and other possibles configurations.'=> 'Usuários e outras configurações possíveis',
+    'Remember me next time' => 'Emit txen em rebmemer',
+    'Assign a class'=>'Alocar Usuário em uma classe',
+    'Assign a user to class'=>'Colocar um usuário em um classe',
+    'List users by systems'=>'Listar os usuários de acordo com o sistema',
+    'Perfomance'=>'Desempenho',
+    'List user perfomance by date'=>'Listar o desempenho de um determinado usuário',
+    'Interation'=>'Interações',
+    'List user interation by date'=>'Listar as interações de um determinado usuário',
+    'Tip and Trick:'=>'Dica e Ajuda:',
+    'User Registed Sucessful with name:'=>'Usuário cadastro efetuado com sucesso de nome: ',
+    'Create'=>'Criar',
+    'Save'=>'Salva',
+    'Password'=>'Senha',
+    'Name'=>'Nome',
+    'Username'=>'Usuário',
+    'Forgotten Password?'=>'Esqueceu a senha?',
+    'Reset your password'=> 'Resete sua senha',
+    'using email address.'=> 'utilizando seu endereço de e-mail.',
+    'Add Matrix' => 'Adicionar Matriz',
+    'Degree'=>'Nível'
+)
+?>
---
+++ app/messages/pt_br/menu.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+    'User' => 'Usuário',
+    'Class' => 'Classes',
+    'Peformance' => 'Desempenho',
+    'Interation' => 'Interação',
+    'Template' => 'Modelos',
+    'Theme' => 'Temas',
+    'Type' => 'Tipos',
+    'Block' => 'Blocos',
+    'Cobject' => 'Cobjeto',
+    'Cobject' => 'Cobjeto',
+    'Goal' => 'Objetivo',
+    'Modality' => 'Modalidade',
+    'Skill' => 'Habilidade',
+    'Matrix' => 'Matriz',
+    'Content'  => 'Conteúdo',
+    'Script'  => 'Roteiro',
+    'Discipline'  => 'Disciplina',
+    'Library'=>'Biblioteca',
+    'Advanced'=>'Avançado',
+    'Properties'=>'Propriedades',
+    'Types'=>'Tipos',
+    'Status'=>'Estatus',
+    'Alias'=>'Alias',
+    'Import/Export'=>'Import/Export',
+)
+?>
