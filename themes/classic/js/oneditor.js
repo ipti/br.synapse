687ca468f32f9998be616f6d5a4fecb075ad0174
Francisco <Francisco@franciscocabral.net>
2013-05-20 09:18:59 -0300

    Reformulação dos métodos existentes e adição do método addImage.

--- themes/classic/js/oneditor.js
+++ themes/classic/js/oneditor.js
@@ -10,38 +10,26 @@ $(function() {
         num_page_links_to_display: 20,
         nav_panel_id : '.navpage',
         editor : newEditor
-    });            
+    }); 
+    
     newEditor.countQuestion['pg0'] = 0;
     newEditor.countTasks['pg0_q0'] = 0;
+    
     $("#toolbar").draggable({
         axis: "y"
     });
                             
     $("#addpage").click(function(){
-        $(".content").append(newEditor.buildHtml('addPage'));
-        $('.canvas').pajinate({
-            items_per_page : 1,
-            nav_label_first : '<<',
-            nav_label_last : '>>',
-            nav_label_prev : '<',
-            nav_label_next : '>',
-            show_first_last : false,
-            num_page_links_to_display: 20,
-            nav_panel_id : '.navpage',
-            editor : newEditor
-        });
+        newEditor.addPage();
     });
-    $("#addtext").click(function(){
-        var id = newEditor.currentTask;
-        $('#'+id).append(newEditor.buildHtml('addText'));
-        $( ".text" ).draggable({
-            containment: '#'+id, 
-            scroll: true
-        });
+    $(".insertText").click(function(){
+        newEditor.addTexto();
+    });
+    $(".insertImage").last().click(function(){
+        newEditor.addImage();
     });
     $("#addquestion").click(function(){
-        var id = newEditor.currentPageId;
-        $('#'+id).append(newEditor.buildHtml('addQuest'));
+        newEditor.addQuest();
     })
     /*$(".page").live("click",(function(){
                     //alert(newEditor.currentPageId);
@@ -55,15 +43,9 @@ $(function() {
                     newEditor.currentQuest = $(this).attr('id');
                 }));*/
     $(".addTask").live("click",(function(){
-        var id = $(this).attr('id');
-        id = id.replace("tsk_", "");
-        newEditor.currentQuest = id;
-        $('#'+id).append(newEditor.buildHtml('addTask'));
+        newEditor.addTask($(this).attr('id'));
     }));
-    $('.task').live("click",function(){
-        $('.task').removeClass('active');
-        var id = $(this).attr('id');
-        $(this).addClass('active');
-        newEditor.currentTask = id;
+    $('.task').live("mousedown",function(){
+        newEditor.changeTask($(this));
     });
 });
