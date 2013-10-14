3b0c4023676b8ae355c056d828498b860c275d77
ruanc_000 <ruanc_000@athens>
2013-10-13 22:10:23 -0300

    Alterações para avaliação e novos arquivos de som

--- app/views/render/stage.php
+++ app/views/render/stage.php
@@ -34,7 +34,7 @@
     NEWRENDER.scriptID = 1;
     NEWRENDER.actorID = <?php echo $actor->id?>;
     NEWRENDER.actorName = '<?php echo $actor->person->name?>';
-    NEWRENDER.atdID = 'exam';
+    NEWRENDER.atdID = 'trial';
     
     $(function() {
         var json = <?php echo $json ?>;
