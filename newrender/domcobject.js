2dab311fe0d0adf150f6c19be92e5b7098224093
ipti <ti@ipti.org.br>
2014-04-03 20:44:29 -0300

    versão funcionando do novo render

--- newrender/domcobject.js
+++ newrender/domcobject.js
@@ -31,7 +31,7 @@ var DomCobject = function(cobject){
                      self.id.screen = this.cobject.screens[this.pos.screen].id;
                      self.dom.append(self.buildScreen());
                };
-               return self.dom.html();
+               return self.dom;
             }
 
             this.buildScreen = function(){
@@ -42,7 +42,7 @@ var DomCobject = function(cobject){
                      self.id.pieceset =  this.cobject.screens[this.pos.screen].piecesets[this.pos.pieceset].id;
                      self.domScreen.append(this.buildPieceSet());
                   };
-                  return self.domScreen.html();
+                  return self.domScreen;
                   //eval('this.buildScreen_'+this.cobject.template);
             }
             
@@ -54,25 +54,44 @@ var DomCobject = function(cobject){
                      self.id.piece =  this.cobject.screens[this.pos.screen].piecesets[this.pos.pieceset].pieces[this.pos.piece].id;
                      self.domPieceSet.append(this.buildPiece());
                   };
-                  return self.domPieceSet.html();
+                  return self.domPieceSet;
             }
             
             this.buildPiece = function(){
                   self.domPiece = $('<div class="piece" id="'+self.id.piece+'"></div>');
+                  self.domPiece.append(self.buildEnum());
                   var elements_length = self.cobject.screens[this.pos.screen].piecesets[self.pos.pieceset].pieces[self.pos.piece].elements.length;
                   for(self.pos.element = 0; self.pos.element < elements_length; self.pos.element++) {
                      self.id.element =  self.cobject.screens[self.pos.screen].piecesets[self.pos.pieceset].pieces[self.pos.piece].elements[self.pos.element].elementID;
                      self.domPiece.append(self.buildElement());
                   };
-                  return self.domPiece.html();
+                  return self.domPiece;
+            }
+
+            this.buildEnum = function(){
+              self.domEnum = $('<div class="enunciation"></div>');
+              eval("self.buildEnum_"+cobject.template_code+"();");
             }
             
              this.buildElement = function(){
                   self.domElement = $('<li class="element" id="'+self.id.element+'"><span>'+self.id.element+'</span></li>');
-                  return self.domElement.html();
+                  eval("self.buildElement_"+cobject.template_code+"();");
+                  return self.domElement;
+            }
+
+            this.buildEnum_MTE = function(){
+              self.domEnum
             }
             
+            this.buildElement_MTE = function(){
+              alert(10);
+            }
+
+            this.runMe = function(){
+              alert(20)
+            }
+
+
            
       }
 
-        
