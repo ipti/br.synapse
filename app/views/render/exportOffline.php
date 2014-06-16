3ffee5288cf448efcb0d1cfe34b76c55e2111909
José Agnaldo Jr. <agjunior.sd@gmail.com>
2014-06-16 15:45:12 -0300

    View de exportação criado

---
+++ app/views/render/exportOffline.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<head>
+        <title>Render</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+
+    <body id="synapse">
+        <form action="" id="">
+			<select id="school">
+			  	<option value="1">1</option>
+			  	<option value="2">2</option>
+			</select>
+			<select multiple id="cobject_block">
+			  	<option value="1">1</option>
+			  	<option value="1">2</option>
+			</select>
+			<input type="submit" value="Baixar" >
+		</form>
+    </body>
+</html>
