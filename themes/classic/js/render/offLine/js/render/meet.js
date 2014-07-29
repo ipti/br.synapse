46bce022f10a0e628be19c6536fb55f52b43cfd0
Francisco Cabral <franciscocabralfilho@gmail.com>
2014-07-29 16:03:26 -0300

    Corrigindo Layout do elementos

--- themes/classic/js/render/offLine/js/render/meet.js
+++ themes/classic/js/render/offLine/js/render/meet.js
@@ -402,13 +402,13 @@ this.Meet = function(options) {
             var ask_answer = $(this).parents('div').attr('class');
             if (ask_answer == 'ask') {
                 if (!$(this).hasClass('ael_clicked')) {
-                    $(this).css('opacity', '0.4');
+                    $(this).css('border', '3px dashed #FBB03B');
                     $(this).siblings().hide();
                     $(this).closest('div.ask').siblings('div.answer').children('div[group]:not(.ael_clicked)').show(500);
                     $(this).addClass('ael_clicked');
                     $(this).addClass('last_clicked');
                 } else {
-                    $(this).css('opacity', '1');
+                    $(this).css('border', '3px dashed transparent');
                     $(this).siblings(':not(.ael_clicked)').show();
                     $(this).closest('div.ask').siblings('div.answer').children('div[group]:not(.ael_clicked)').hide(500);
                     $(this).removeClass('ael_clicked');
