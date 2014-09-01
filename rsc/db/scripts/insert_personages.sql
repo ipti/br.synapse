44d1d14e6344663ee74b39390aca687234e162e6
ipti002 <ipti002@ipti002-desktop>
2014-09-01 10:35:58 -0300

    Reorganização das pastas 2.0

--- rsc/db/scripts/insert_personages.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM synapse.personage
-LIMIT 0, 1000
-
--- Date: 2013-05-13 10:31
-*/
-INSERT INTO `personage` (`ID`,`organizationID`,`name`) VALUES (1,3,'Aluno');
-INSERT INTO `personage` (`ID`,`organizationID`,`name`) VALUES (2,3,'Tutor');
