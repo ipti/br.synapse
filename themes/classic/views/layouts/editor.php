ed10bd2eb40c96e21b224bfac6b3c60856df7106
franciscocabral <franciscocabralfilho@gmail.com>
2015-04-22 10:25:55 -0300

    Modificações no Layout [2] Signed-off-by:franciscocabral <franciscocabralfilho@gmail.com>

--- themes/classic/views/layouts/editor.php
+++ themes/classic/views/layouts/editor.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/fa/css/font-awesome.min.css">
         <!-- blueprint CSS framework -->
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/reset.css" media="screen, projection" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/print.css" media="print" />
