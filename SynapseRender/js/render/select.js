595fe087811393f6e2e38821c163213e2153ed9e
fabio-ipti <ns.ffabio@gmail.com>
2015-07-29 18:00:37 -0300

    Construção da funcionalidade de Registrar Alunos quando não fora selecionado algum no Renderizador.

--- SynapseRender/js/render/select.js
+++ SynapseRender/js/render/select.js
@@ -1,56 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var DB_synapse = new DB();
- 
+
     //Classe Principal
-    var getAllClassAndStudents = function(){
+    var getAllClassAndStudents = function () {
         DB_synapse.actorOwnUnity = new Array();
-        DB_synapse.getAllClass(DB_synapse.getAllStudentFromClasses,eventFilterMeet);
+        DB_synapse.getAllClass(DB_synapse.getAllStudentFromClasses, eventFilterMeet);
     }
 
-    var eventFilterMeet = function (actorOwnUnity){
+    var eventFilterMeet = function (actorOwnUnity) {
         var classrooms = actorOwnUnity;
-         
-        if(sessionStorage.getItem('login_personage_name') == 'Tutor'){
+
+        if (sessionStorage.getItem('login_personage_name') === 'Tutor') {
             $("#login-select").show();
 
             $("#classroom").html('');
-            $.each(classrooms, function(i, v){
-                $("#classroom").append("<option value='"+v['unity_id']+"' id='"+i+"' >"+v['unity_name']+"</option>");
+            $("#classroom").append("<option value='null'>Selecione uma Turma</option>");
+            $.each(classrooms, function (i, v) {
+                $("#classroom").append("<option value='" + v['unity_id'] + "' id='" + i + "' >" + v['unity_name'] + "</option>");
             });
 
-            $('#classroom').change(function(){
-                $('#actor').html('');
-                var cr = $('#classroom option:selected').attr('id');
-               
-                $.each(classrooms[cr]['actors'], function(i, v){
-                    $('#actor').append("<option value='"+v['actor_id']+"''>"+v['actor_name']+"</option>");
-                })
+            $('#classroom').change(function () {
+                var val = $('#classroom option:selected').val();
+                
+                if (val != 'null') {
+                    $('#actor').html('');
+                    var cr = $('#classroom option:selected').attr('id');
+                    $('#actor').append("<option value='null'>Selecione um Aluno</option>");
+                    $.each(classrooms[cr]['actors'], function (i, v) {
+                        $('#actor').append("<option value='" + v['actor_id'] + "''>" + v['actor_name'] + "</option>");
+                    });
+                }
             });
-            $("#classroom").trigger('change');
         } else {
             $("#login-select").hide();
-            $("#select-discipline").css('margin-top', '10%')
+            $('#select-student').hide();
+            $("#select-discipline").css('margin-top', '10%');
         }
 
-        $('.discipline').click(function(){
+        $('.discipline').click(function () {
 
             sessionStorage.setItem('id_discipline', $(this).attr('discipline'));
-            if(sessionStorage.getItem('login_personage_name') == 'Tutor'){
+            if (sessionStorage.getItem('login_personage_name') == 'Tutor') {
                 sessionStorage.setItem('id_actor', $('#actor').val());
                 sessionStorage.setItem('name_actor', $('#actor').find(":selected").text());
             }
             window.location = "./meet.html";
         });
     }
-    
-    if(sessionStorage.getItem('login_personage_name') == 'Tutor'){
+
+    if (sessionStorage.getItem('login_personage_name') === 'Tutor') {
         getAllClassAndStudents();
-    }else{
+    } else {
         eventFilterMeet(null);
     }
-    
-    
-    $('#toolsAdmin').on('click', function(){
+
+
+    $('#toolsAdmin').on('click', function () {
         location.href = "import.html";
     });
 
