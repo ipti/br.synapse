673656d10d605df8f1c6cc570657f2a8c96e8466
Rafael Pedral <rafael.pedral@gmail.com>
2016-01-06 15:53:47 -0300

    Enhancement #9

--- Editor/app/views/cobjectCobjectblock/_form.php
+++ Editor/app/views/cobjectCobjectblock/_form.php
@@ -1,30 +1,42 @@
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'cobject-cobjectblock-form',
-	'enableAjaxValidation'=>false,
-)); ?>
-        <div class="panelGroup form">
-            <?php echo $form->errorSummary($model); ?>
-            <div class="panelGroupHeader"><div class=""> <?php echo $title; ?>
-</div></div>
-            <div class="panelGroupBody">
-                <div class="panelGroupAbout">
-                     <?php echo Yii::t('default', 'Fields with * are required.')?></div>
+<?php
+$form=$this->beginWidget('CActiveForm', array(
+    'id'=>'cobject-cobjectblock-form',
+    'enableAjaxValidation'=>false,
+));
+?>
 
-                                    <div class="formField">
-                        <?php echo $form->labelEx($model,'cobject_id'); ?>
-                        <?php echo $form->dropdownlist($model,'cobject_id', chtml::listdata(Cobject::model()->findAll(), 'id', 'id')); ?>
-                        <?php echo $form->error($model,'cobject_id'); ?>
-                    </div>
+<?php
+$baseUrl = Yii::app()->theme->baseUrl;
+$cs = Yii::app()->getClientScript();
+$cs->registerScriptFile($baseUrl . "/js/cobjectCobjectblock/cobjectCobjectblock.js");
+?>
 
-                                        <div class="formField">
-                        <?php echo $form->labelEx($model,'cobject_block_id'); ?>
-                        <?php echo $form->dropdownlist($model,'cobject_block_id', chtml::listData(Cobjectblock::model()->findAll(), 'id', 'name')); ?>
-                        <?php echo $form->error($model,'cobject_block_id'); ?>
-                    </div>
-
-                                    <div class="formField buttonWizardBar">
-                    <?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('default', 'Create') : Yii::t('default', 'Save'),array('class' => 'buttonLink button')); ?>
-                </div>
-                <?php $this->endWidget(); ?>
-            </div>
+<div class="panelGroup form">
+    <?php echo $form->errorSummary($model); ?>
+    <div class="panelGroupHeader">
+        <div class="">
+            <?php echo $title; ?>
         </div>
+    </div>
+    <div class="panelGroupBody">
+        <div class="panelGroupAbout">
+            <?php echo Yii::t('default', 'Fields with * are required.')?>
+        </div>
+        <div class="formField">
+            <?php echo $form->labelEx($model,'cobject_id'); ?>
+            <?php echo $form->dropdownlist($model,'cobject_id', chtml::listdata(Cobject::model()->findAll(), 'id', 'id')); ?>
+            <?php echo $form->error($model,'cobject_id'); ?>
+        </div>
+
+        <div class="formField">
+            <?php echo $form->labelEx($model,'cobject_block_id'); ?>
+            <?php echo $form->dropdownlist($model,'cobject_block_id', chtml::listData(Cobjectblock::model()->findAll(), 'id', 'name')); ?>
+            <?php echo $form->error($model,'cobject_block_id'); ?>
+        </div>
+
+        <div class="formField buttonWizardBar">
+            <?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('default', 'Create') : Yii::t('default', 'Save'),array('class' => 'buttonLink button')); ?>
+        </div>
+        <?php $this->endWidget(); ?>
+    </div>
+</div>
---
+++ Editor/themes/classic/js/cobjectCobjectblock/cobjectCobjectblock.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('#CobjectCobjectblock_cobject_id').select2();
+    $('#CobjectCobjectblock_cobject_block_id').select2();
+});
